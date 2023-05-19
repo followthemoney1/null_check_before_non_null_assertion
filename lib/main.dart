be43823a8dd148b037478afb8778088ba8d34d48
dmytro diachenko <dmytro.diachenko@spsoft.com>
2023-05-19 12:25:03 +0100

    latest changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,9 @@
 void main() {
   print('hello world');
 
-  int? s;
-  s!.bitLength;
-
   String? giftCode;
   final s2 = giftCode != null ? giftCode : '';
 
-  if (s != null) {
-    s.hashCode;
-  }
   String? email;
   if (email.isNotNullNorEmpty) {
     email!.length;
@@ -24,13 +18,16 @@ void main() {
   if (email3 != null) {
     email3!.length;
   }
+
+  int? s;
+  s!.bitLength;
+  if (s != null) {
+    s.hashCode;
+  }
 }
 
 extension NullableStringExtensionsNull<E> on String? {
   /// Returns `true` if this string is `null` or empty.
-  bool get isNullOrEmpty {
-    return this?.isEmpty ?? true;
-  }
 
   /// Returns `true` if this string is not `null` and not empty.
   bool get isNotNullNorEmpty {
@@ -39,6 +36,10 @@ extension NullableStringExtensionsNull<E> on String? {
 
   bool get isNotEmptyOrFalse {
     return this?.isNotEmpty ?? false;
+  }
+
+  bool get isNullOrEmpty {
+    return this?.isEmpty ?? true;
   }
 }
 
--- null_check_lint/lib/null_check_lint.dart
+++ null_check_lint/lib/null_check_lint.dart
@@ -1,14 +1,19 @@
+import 'dart:math';
+
 import 'package:analyzer/dart/ast/ast.dart';
 import 'package:analyzer/dart/ast/token.dart';
 import 'package:analyzer/dart/element/element.dart';
 import 'package:analyzer/dart/element/nullability_suffix.dart';
+import 'package:analyzer/dart/element/type.dart';
 import 'package:analyzer/error/error.dart';
 import 'package:analyzer/error/listener.dart';
 import 'package:custom_lint_builder/custom_lint_builder.dart';
+import 'package:logger/logger.dart';
 
 /// This is the entrypoint of our plugin.
 /// All plugins must specify a `createPlugin` function in their `lib/<package_name>.dart` file
 PluginBase createPlugin() => _NullCheckableLint();
+var logger = Logger();
 
 /// The class listing all the [LintRule]s and [Assist]s defined by our plugin.
 class _NullCheckableLint extends PluginBase {
@@ -32,19 +37,95 @@ class NullCheckBeforeNonNullAssertion extends DartLintRule {
 
   @override
   void run(CustomLintResolver resolver, ErrorReporter reporter, CustomLintContext context) {
-    bool expressionExist = false;
+    List latestVariable = [];
+    // bool expressionExist = true;
+    // context.registry.addPrefixExpression((node) {
+    //   reporter.reportErrorForNode(code, node);
+    // });
+
+    // context.registry.addPropertyAccess((node) {
+    //   final target = node.target;
+    //
+    //   final getter = node.propertyName is SimpleIdentifier && (node.propertyName as SimpleIdentifier).inGetterContext();
+    //
+    //   if (node is! SimpleIdentifier) {
+    //     reporter.reportErrorForNode(code, node);
+    //     return;
+    //   }
+    //   if (getter is! PropertyAccessorElement) {
+    //     reporter.reportErrorForNode(code, node);
+    //     return;
+    //   }
+    // });
     context.registry.addIfStatement((node) {
-      final condition = node.expression;
-      if (condition is Expression) {
-        expressionExist = true;
-        //!reqOperators || additionalParams ||
+      var condition = node.expression;
+      var getter = condition.staticParameterElement;
+
+      // The accessor must be a getter, and it must be synthetic.
+      // if (!(getter!.isGetter || getter!.isSynthetic)) {
+      //   return;
+      // }
+
+      // The variable must be not synthetic, and have to setter yet.
+      // var variable = getter.variable;
+      // if (variable.isSynthetic || variable.setter != null) {
+      //   return;
+      // }
+      // if (condition is PropertyAccess) {
+      //   reporter.reportErrorForNode(code, node);
+      // }
+      if (condition is BinaryExpression) {
+        final leftOperand = condition.leftOperand;
+        final rightOperand = condition.rightOperand;
+
+        //mark: checking the tpe of expression, if type is not equall [!=]
+        final operand = [
+          TokenType.BANG_EQ,
+          TokenType.QUESTION,
+          TokenType.EQ_EQ,
+          TokenType.QUESTION_PERIOD,
+          TokenType.QUESTION_QUESTION,
+          // TokenType.all
+        ].contains(condition.operator.type);
+        final additionalParams = rightOperand is Null;
+
+        if (leftOperand is SimpleIdentifier) {
+          // if (operand || additionalParams) {
+          //   //!reqOperators || additionalParams ||
+          //   latestVariable.add(leftOperand.name);
+          //   // reporter.reportErrorForNode(code, node);
+          // } else {
+          //   latestVariable.remove(leftOperand.name);
+          // }
+        } else if (rightOperand is LibraryIdentifier) {
+          reporter.reportErrorForNode(code, node);
+        }
       }
+      // else if (node is! PropertyAccessorElement) {
+      //   if (getter != null) {
+      //     reporter.reportErrorForNode(code, node);
+      //     latestVariable.remove(getter.name);
+      //   }
+      // }
+      // final target = node.target;
+      //
+      // final getter = node.propertyName is SimpleIdentifier && (node.propertyName as SimpleIdentifier).inGetterContext();
+      //
+      // if (node is! SimpleIdentifier) {
+      //   reporter.reportErrorForNode(code, node);
+      //   return;
+      // }
+      // if (getter is! PropertyAccessorElement) {
+      //   reporter.reportErrorForNode(code, node);
+      //   return;
+      // }
+      // reporter.reportErrorForNode(code, node);
     });
     context.registry.addIfElement((node) {
       final condition = node.expression;
-      if (condition is Expression) {
-        expressionExist = true;
-        //!reqOperators || additionalParams ||
+      if (condition is ConditionalExpression) {
+        // condition = (node.expression as NullLiteral).;
+        reporter.reportErrorForNode(code, node);
       }
     });
     //else we doing check
@@ -55,7 +136,7 @@ class NullCheckBeforeNonNullAssertion extends DartLintRule {
         if (element is VariableElement) {
           final offset = node.operator.offset;
           final length = node.operator.length;
-          if (!expressionExist)
+          if (latestVariable.contains(element.name))
             // final sourceRange = SourceRange(offset, length);
             reporter.reportErrorForOffset(code, offset, length);
         }
